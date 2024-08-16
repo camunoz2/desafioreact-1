bf2d59cb82cd7facf52d31e626cff3960a240fc9
Cristian M <camunoz2@gmail.com>
2024-08-16 11:23:46 -0400

    actualize functiones y simplifique codigo

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -8,71 +8,30 @@ import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
 import { numberToClpPrice } from "../utils/transformPrice";
 
-function Adder({ count, add, remove }) {
-  return (
-    <Stack direction="horizontal">
-      <Button
-        onClick={add}
-        className="bg-primary text-center flex-grow-1 text-white"
-      >
-        +
-      </Button>
-      <div className="text-center flex-grow-1">{count}</div>
-      <Button
-        onClick={remove}
-        className="bg-primary text-center flex-grow-1 text-white"
-      >
-        -
-      </Button>
-    </Stack>
-  );
-}
-
-function PizzaItem({ item, handleAdd, handleRemove }) {
-  return (
-    <Row className="align-items-center border border-primary rounded overflow-hidden">
-      <Col style={{ padding: 0 }}>
-        <Image src={item.img} alt={item.name} style={{ height: "200px" }} />
-      </Col>
-      <Col>
-        <h2>{item.name.toUpperCase()}</h2>
-      </Col>
-      <Col>
-        <Adder add={handleAdd} remove={handleRemove} count={item.count} />
-      </Col>
-      <Col className="text-center">
-        <p>{numberToClpPrice(item.price * item.count)}</p>
-      </Col>
-    </Row>
-  );
-}
-
 export default function Cart({ pizzaCart, navHeight, footerHeight }) {
+  const totalValue = 200;
   const [cart, setCart] = useState(pizzaCart);
 
-  const totalValue = cart.reduce(
-    (total, item) => total + item.price * item.count,
-    0
-  );
-
-  function handleAdd(id) {
-    setCart((prevCart) =>
-      prevCart.map((item) =>
-        item.id === id ? { ...item, count: item.count + 1 } : item
-      )
-    );
+  function add(id) {
+    let updatedCart = cart.map((item) => {
+      if (item.id === id) {
+        return { ...item, count: item.count + 1 };
+      }
+      return item;
+    });
+    setCart(updatedCart);
   }
 
-  function handleRemove(id) {
-    setCart((prevCart) =>
-      prevCart
-        .map((item) =>
-          item.id === id && item.count > 1
-            ? { ...item, count: item.count - 1 }
-            : item
-        )
-        .filter((item) => item.count > 0)
-    );
+  function remove(id) {
+    let updatedCart = cart.map((item) => {
+      if (item.id === id) {
+        return { ...item, count: item.count - 1 };
+      }
+      return item;
+    });
+
+    updatedCart = updatedCart.filter((item) => item.count > 0);
+    setCart(updatedCart);
   }
 
   return (
@@ -84,12 +43,41 @@ export default function Cart({ pizzaCart, navHeight, footerHeight }) {
     >
       <Stack gap={3}>
         {cart.map((item) => (
-          <PizzaItem
-            handleAdd={() => handleAdd(item.id)}
-            handleRemove={() => handleRemove(item.id)}
-            item={item}
+          <Row
             key={item.id}
-          />
+            className="align-items-center border border-primary rounded overflow-hidden"
+          >
+            <Col style={{ padding: 0 }}>
+              <Image
+                src={item.img}
+                alt={item.name}
+                style={{ height: "200px" }}
+              />
+            </Col>
+            <Col>
+              <h2>{item.name.toUpperCase()}</h2>
+            </Col>
+            <Col>
+              <Stack direction="horizontal">
+                <Button
+                  onClick={() => add(item.id)}
+                  className="bg-primary text-center flex-grow-1 text-white"
+                >
+                  +
+                </Button>
+                <div className="text-center flex-grow-1">{item.count}</div>
+                <Button
+                  onClick={() => remove(item.id)}
+                  className="bg-primary text-center flex-grow-1 text-white"
+                >
+                  -
+                </Button>
+              </Stack>
+            </Col>
+            <Col className="text-center">
+              <p>{numberToClpPrice(item.price * item.count)}</p>
+            </Col>
+          </Row>
         ))}
       </Stack>
       <div className="display-6 d-flex justify-content-end">
